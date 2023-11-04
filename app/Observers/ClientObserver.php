0ed98198ff9f9723eb4e202bd8354943d842f96d
dimasm93 <software.dimas_m@icloud.com>
2023-11-04 15:04:52 +0700

    nyalip

--- app/Observers/ClientObserver.php
+++ app/Observers/ClientObserver.php
@@ -41,26 +41,25 @@ class ClientObserver
      *
      * @param  \App\Client $data
      * @return void
-    
+    */
     public function Created(Client $client)
     {
         $this->pushToKafka($client);
         //dd($client);
     }
-     */
+
 
     /**
      * Handle the inventory "updated" event.
      *
      * @param  \App\Client $data
      * @return void
-     
-    
+    */
     public function Saved(Client $client)
     {
         $this->pushToKafka($client);
     }
-    */
+
 
     /**
      * Handle the inventory "deleted" event.
@@ -68,12 +67,11 @@ class ClientObserver
      * @param  \App\Client $client
      * @return void
     
-     
+     */
     public function deleted(Client $client)
     {
         $this->pushToKafka($client);
     }
-    */
 
     /**
      * Push inventory to kafka
